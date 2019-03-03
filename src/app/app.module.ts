505d83df8d7dbe02b4d3c0b4d725d8d785e490f2
Will <15718706+willdyess@users.noreply.github.com>
2019-03-03 12:34:18 -0500

    v0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConsultingComponent } from './consulting/consulting.component';
+import { SoftwareComponent } from './software/software.component';
+import { ModeComponent } from './mode/mode.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { PanelModule, AutoCompleteModule, LayoutModule, MenuModule, TabsModule } from '@eamode/eang';
+import { CtaComponent } from './cta/cta.component';
+import { NewsComponent } from './components/news/news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConsultingComponent,
+    SoftwareComponent,
+    ModeComponent,
+    BannerComponent,
+    CtaComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PanelModule,
+    AutoCompleteModule,
+    LayoutModule,
+    MenuModule,
+    TabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/software/software.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { EangElement } from '@eamode/eang';
+
+@Component({
+  selector: 'app-software',
+  templateUrl: './software.component.html',
+  styleUrls: ['./software.component.scss']
+})
+export class SoftwareComponent implements OnInit {
+  tabActivate$$ = new Subject<EangElement>();
+
+  example = {
+    name: '',
+    isHidden: true,
+    children: [
+      {
+        name: 'Supplies',
+        icon: 'ea-edit',
+        iconStyle: 'ea-negative',
+        children: [
+          {
+            name: 'Inventory'
+          },
+        ]
+      },
+      {
+        name: 'Orders',
+        icon: 'ea-folder-plus',
+        iconStyle: 'ea-negative'
+      },
+      {
+        name: 'Inventory',
+        icon: 'ea-database',
+        iconStyle: 'ea-negative'
+      },
+      {
+        name: 'Vendors',
+        icon: 'ea-download',
+        iconStyle: 'ea-negative'
+      }
+    ]
+  };
+  constructor() {}
+
+  ngOnInit() {}
+}
