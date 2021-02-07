21589045c03aa6ad2e4a50af11523811ba78b15d
Sebastian <sebastian@kropponline.de>
2021-02-06 19:30:04 -0500

    bootstrap

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+module.exports = ctx => {
+  const config = {
+    map: ctx.options.map,
+    plugins: [
+      require('postcss-import')(),
+      require('postcss-mixins')(),
+      require('postcss-nested')(),
+    ],
+  }
+  if(ctx.env === 'production'){
+    config.plugins.push(require('cssnano')())
+  }
+  return config
+}
